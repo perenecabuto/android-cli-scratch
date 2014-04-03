392c1a62ce09b7d8b96b7f8641333af1656b869a
Felipe Ramos Ferreira <perenecabuto@gmail.com>
2014-04-03 02:04:25 -0300

    Removing trash files

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/bacamarte/workspace/android-scratch/bin/AndroidManifest.xml : \
-/home/bacamarte/workspace/android-scratch/AndroidManifest.xml \
--- bin/scratch-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/bacamarte/workspace/android-scratch/bin/scratch-debug-unaligned.apk : \
-/home/bacamarte/workspace/android-scratch/bin/scratch.ap_ \
-/home/bacamarte/workspace/android-scratch/bin/classes.dex \
